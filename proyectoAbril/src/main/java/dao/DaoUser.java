485cb63b97813702d8ee138d25de71923b5d1c0a
Umpa0o <patfedriani@hotmail.com>
2024-05-06 13:35:37 +0200

    llistadoUser para admin
    no implementado

--- proyectoAbril/src/main/java/dao/DaoUser.java
+++ proyectoAbril/src/main/java/dao/DaoUser.java
@@ -5,10 +5,15 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+
+import com.google.gson.Gson;
 
 import modelo.User;
 /**CLASE DAO: clase reservada para todas las acciones que tiene 
- * un objeto con iteración en una base de datos
+ * un objeto con iteración en una base de datos. Tienen persistencia. DAO -> InsertarUsuario
+ * 
+ * clase TAD: clases creadas para gestionar colecciones de objeto.Ej un ArrayList. TAD -> ListarUsuarios
  * 
  * */
 
@@ -31,7 +36,7 @@ public class DaoUser {
 		
 		
 		/**Metodo para insertarUsuarios en la database
-		 * @param ue del objeto User
+		 * @param Objeto ue del tipo User
 		 * @throws SQLException
 		 * */
 		public void insertarUser(User ue) {
@@ -95,34 +100,62 @@ public class DaoUser {
 				System.out.println("Error excepcion listarUsuarios"+e.getMessage());
 			}
 			
-		}//fin metodo insertarUser
+		}//fin metodo insertarUser()
 		
-		/**metodo listar
+		/**metodo listarUsuarios() Crea una coleccion de datos tipo User a null
 		 * 
 		 * */
 		
-		public void listarUsuarios() {
-			
-			ResultSet rsLis=null;
+		public ArrayList<User> listarUsuarios() {
+			ArrayList<User> listaUe=null;
 			try {
-				Statement st = con.createStatement();
-				String query = "SELECT * FROM usuario";
 				
-				rsLis = st.executeQuery(query);
-				System.out.println("TABLA USUARIOS");
+				String consulta = "SELECT * FROM usuario";
+				PreparedStatement pr =con.prepareStatement(consulta);
+				ResultSet rs = pr.executeQuery();	
 				
-				while(rsLis.next()) {
-					System.out.println(rsLis.getString(1)+"\t" +rsLis.getString(2)+"\t" +rsLis.getString(3)+"\t" +
-				rsLis.getString(4)+"\t" +rsLis.getString(5)+"\t" +rsLis.getString(6)+"\t" +rsLis.getString(7)+"\t" 
-				+rsLis.getString(8)+"\t" +rsLis.getString(9));
-				}
+				while(rs.next()) {
+					//si es null lo creamos y si no le añadimos información para no tener que resetearlo
+					if(listaUe==null) {
+						listaUe = new ArrayList<User>();
+					}//fin if
+					
+					listaUe.add(new User(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getString(4), 
+							rs.getInt(5), rs.getString(6), rs.getString(7), rs.getString(8)));
+					
+				}//fin while
 				
-	
-			} catch (SQLException e) {
+			}catch(SQLException ex) {
+				ex.getMessage();
 				
-				System.out.println("Error excepcion listarUsuarios"+e.getMessage());
-			}
+			}//fin try/catch
 			
-		}
+			return listaUe;
+			
+		}// fin listarUsuarios()
+		
+		
+		/**Método listarUeJson() nos comunicaremos con el cliente con json, diferenciando el front del back
+		 * 	hacemos que ntro servlet cree un JSON para devolverle al cliente.
+		 * 	Para Java el contenido de un json es tipo String
+		 * 
+		 * @build gson.google
+		 * 
+		 * **/
+		public String listarUeJson() {
+			//generamos cadena vacia
+			String json ="";
+			//creamos libreria/objeto json q nos ayudara a convertir el arrayList en json
+			Gson gson = new Gson();
+			
+			json = gson.toJson(this.listarUsuarios());
+			
+			return json;
+			
+			
+			
+		}// fin listarUeJson()
+		
+		
 		
 }//fin clase DaoUser
