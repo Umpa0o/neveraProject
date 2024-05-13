f8fe1798107b3bfb920a73b26d037f1d78e0f21e
Umpa0o <patfedriani@hotmail.com>
2024-05-13 11:38:39 +0200

    workupdate

---
+++ proyectoAbril/src/main/webapp/jS/editUs.js
@@ -0,0 +1,16 @@
+//EVENTOS
+	//devuelve una coleccion de elementos en este caso tipo button y que coja el primero que es i=0
+	//const btnAgregar = document.querySelector("btnAgregar");
+	
+	document.addEventListener('DOMContentLoaded', function(){
+		const btnAgregar = document.getElementsByTagName("button")[0];
+
+		//capturamos el evento de hacer click
+		btnAgregar.addEventListener('click', function(){
+			alert("patata");
+			
+			larala();
+	
+		});
+		
+	})//fin doc addEvent
